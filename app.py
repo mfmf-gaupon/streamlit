0163c147c28eb30434d3ec6402558c4aff85efc1
gaupon <shun27182@yahoo.co.jp>
2021-11-19 22:35:31 +0900

    package test

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import streamlit as st
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import gym
+import mujoco_py
 
 def main():
     st.write("Hello World!")
     print(tf.__version__)
+    st.write(tf.__version__)
 
 if __name__ == "__main__":
     main()
