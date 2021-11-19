08ba14fd6819585c75806b218db5312cd0fce78b
gaupon <shun27182@yahoo.co.jp>
2021-11-19 22:08:36 +0900

    package test

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import gym
 
 def main():
     st.write("Hello World!")
+    print(tf.__version__)
 
 if __name__ == "__main__":
     main()
