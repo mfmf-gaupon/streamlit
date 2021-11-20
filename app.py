a90e1132e9a0e17eff34391168ca383534b0ac34
gaupon <shun27182@yahoo.co.jp>
2021-11-20 12:58:55 +0900

    hello world

--- app.py
+++ app.py
@@ -3,13 +3,9 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import gym
 import numpy as np
-import mujoco_py
 
 def main():
     st.write("Hello World!")
-    print(tf.__version__)
-    st.write(tf.__version__)
-    st.write(np.__version__)
 
 if __name__ == "__main__":
     main()
